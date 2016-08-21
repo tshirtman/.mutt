bdc554c387fcb61c45c8aae6cea4cb05ff04ffdb
gabriel pettier <dev@tangibledisplay.com>
2016-08-21 23:26:23 +0200

    build neomutt from debian source package

--- init.sh
+++ init.sh
@@ -46,9 +46,29 @@ done
 
 while true
 do
-read -p "do you want to install packages (isync, mutt-patches notmuch-mutt gnupg2 jq tmux imapclient urlscan)" yn
+read -p "do you want to build neomutt?" yn
 case $yn in
-	[Yy]*) sudo apt-get install isync mutt-patched notmuch-mutt gnupg2 jq tmux;
+	[Yy]*) sudo apt-get install debhelper dh-autoreconf docbook-xml docbook-xsl libgnutls28-dev libgpgme11-dev libidn11-dev libkrb5-dev libncursesw5-dev libsasl2-dev libtokyocabinet-dev xsltproc
+		pushd /tmp
+		git clone https://anonscm.debian.org/git/pkg-mutt/mutt.git || (pushd mutt; git pull; popd)
+		wget http://http.debian.net/debian/pool/main/m/mutt/mutt_$(head -n 1 mutt/debian/changelog |sed -s 's/.*(\(.*\)-.*/\1/').orig.tar.gz
+		pushd mutt
+		dpkg-buildpackage
+		popd
+		sudo dpkg -i mutt*.deb
+		sudo apt install -f
+		popd
+		break;;
+	[Nn]*) break;;
+	*) echo "please answer yes or no";;
+esac
+done
+
+while true
+do
+read -p "do you want to install packages (isync, gnupg2 jq tmux imapclient urlscan)" yn
+case $yn in
+	[Yy]*) sudo apt-get install isync gnupg2 jq tmux;
 		pip install --user imapclient urlscan
 		break;;
 	[Nn]*) break;;
