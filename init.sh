b551f01aa2718f6083f32186371cdba7bc5a23db
Gabriel Pettier <gabriel@tangibledisplay.com>
2017-10-08 16:50:09 +0200

    update init.sh

--- init.sh
+++ init.sh
@@ -3,16 +3,6 @@
 
 while true
 do
-read -p "do you want to link muttrc to ~/.muttrc (Y/N)" yn
-case $yn in
-	[Yy]*) ln -s $PWD/muttrc ~/.muttrc; break;;
-	[Nn]*) break;;
-	*) echo "please answer yes or no";;
-esac
-done
-
-while true
-do
 read -p "do you want to link mbsyncrc to ~/.mbsyncrc (Y/N)" yn
 case $yn in
 	[Yy]*) ln -s $PWD/mbsyncrc ~/.mbsyncrc; break;;
@@ -43,26 +33,6 @@ case $yn in
 esac
 done
 
-
-while true
-do
-read -p "do you want to build neomutt?" yn
-case $yn in
-	[Yy]*) sudo apt-get install debhelper dh-autoreconf docbook-xml docbook-xsl libgnutls28-dev libgpgme11-dev libidn11-dev libkrb5-dev libncursesw5-dev libsasl2-dev libtokyocabinet-dev xsltproc w3m libnotmuch-dev python-notifier2
-		pushd /tmp
-		git clone https://anonscm.debian.org/git/pkg-mutt/mutt.git || (pushd mutt; git pull; popd)
-		wget http://http.debian.net/debian/pool/main/m/mutt/mutt_$(head -n 1 mutt/debian/changelog |sed -s 's/.*(\(.*\)-.*/\1/').orig.tar.gz
-		pushd mutt
-		dpkg-buildpackage
-		popd
-		sudo dpkg -i mutt*.deb
-		sudo apt install -f
-		popd
-		break;;
-	[Nn]*) break;;
-	*) echo "please answer yes or no";;
-esac
-done
 
 while true
 do
