14922394ba5a92e03aff6431d13ead0262550f5d
Gabriel Pettier <gabriel@tangibledisplay.com>
2018-11-08 02:18:35 +0100

    use imap_utf7 to make nicer links

--- links.py
+++ links.py
@@ -4,11 +4,13 @@ from os import listdir
 from os.path import join, isdir, exists
 from subprocess import check_output
 from re import fullmatch
+from imapclient import imap_utf7
 
 
-for f in listdir('[Gmail]'):
-    if fullmatch('\..*', f) and isdir(join('[Gmail]', f)):
+for f in listdir(b'[Gmail]'):
+    if fullmatch(b'\..*', f) and isdir(join(b'[Gmail]', f)):
+        transcoded = imap_utf7.decode(f[1:]).encode('utf8')
         if not exists(f[1:]):
-            check_output(['ln', '-s', join('[Gmail]', f), f[1:]])
+            check_output(['ln', '-s', join(b'[Gmail]', f), transcoded])
         else:
             print("{} already exists...".format(f[1:]))
